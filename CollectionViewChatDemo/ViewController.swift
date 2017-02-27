3d1e28d3ccf378c1a1088608030de143465a5305
Vadim Drobinin <vadim@drobinin.com>
2017-02-27 14:27:41 +0300

    Initial Commit

---
+++ CollectionViewChatDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewChatDemo
+//
+//  Created by Vadim Drobinin on 27/2/17.
+//  Copyright © 2017 Vadim Drobinin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
