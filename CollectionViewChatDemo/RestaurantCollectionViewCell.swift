84f5a9746dba4937f891bcc06edb934a03eeba03
Vadim Drobinin <vadim@drobinin.com>
2017-03-05 19:57:40 +0300

    Add IBDesignables to manage drop shadow and rounded corners

--- CollectionViewChatDemo/RestaurantCollectionViewCell.swift
+++ CollectionViewChatDemo/RestaurantCollectionViewCell.swift
@@ -8,8 +8,47 @@
 
 import UIKit
 
-class RestaurantCollectionViewCell: UICollectionViewCell {
+extension UIView {
   
+  @IBInspectable var shadow: Bool {
+    get {
+      return layer.shadowOpacity > 0.0
+    }
+    set {
+      if newValue == true {
+        self.addShadow()
+      }
+    }
+  }
+  
+  @IBInspectable var cornerRadius: CGFloat {
+    get {
+      return self.layer.cornerRadius
+    }
+    set {
+      self.layer.cornerRadius = newValue
+      
+      // Don't touch the masksToBound property if a shadow is needed in addition to the cornerRadius
+      if shadow == false {
+        self.layer.masksToBounds = true
+      }
+    }
+  }
+  
+  
+  func addShadow(shadowColor: CGColor = UIColor.black.cgColor,
+                 shadowOffset: CGSize = CGSize(width: 1.0, height: 2.0),
+                 shadowOpacity: Float = 0.4,
+                 shadowRadius: CGFloat = 3.0) {
+    layer.shadowColor = shadowColor
+    layer.shadowOffset = shadowOffset
+    layer.shadowOpacity = shadowOpacity
+    layer.shadowRadius = shadowRadius
+  }
+}
+
+class RestaurantCollectionViewCell: UICollectionViewCell {
+
   @IBOutlet weak var restaurantImage: UIImageView!
   @IBOutlet weak var restaurantNameLabel: UILabel!
   @IBOutlet weak var restaurantAddressLabel: UILabel!
@@ -21,8 +60,6 @@ class RestaurantCollectionViewCell: UICollectionViewCell {
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    self.contentView.layer.cornerRadius = 15.0
-    self.contentView.layer.masksToBounds = true
   }
 
 }
