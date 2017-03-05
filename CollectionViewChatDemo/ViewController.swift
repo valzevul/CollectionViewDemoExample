1f997d8b67642a10feb88e7dd65e882a8bf39bde
Vadim Drobinin <vadim@drobinin.com>
2017-03-05 18:59:05 +0300

    Add datastructure for restaurants' objects; add connection between Storyboard and code

--- CollectionViewChatDemo/ViewController.swift
+++ CollectionViewChatDemo/ViewController.swift
@@ -8,8 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct RestaurantDescription {
+  let name: String
+  let imageName: String
+  let address: String
+  let type: String
+  let description: String
+  let providerName: String
+  let providerImageName: String
+}
 
+extension ViewController: UICollectionViewDelegate {
+  
+}
+
+extension ViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return data.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RestaurantCollectionViewCell", for: indexPath) as! RestaurantCollectionViewCell
+    return cell
+  }
+
+}
+
+class ViewController: UIViewController {
+  @IBOutlet weak var collectionView: UICollectionView! {
+    didSet {
+      collectionView.delegate = self
+      collectionView.dataSource = self
+    }
+  }
+  
+  let data = [RestaurantDescription]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
