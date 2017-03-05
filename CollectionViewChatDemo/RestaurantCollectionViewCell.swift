8bd553fa179bcdd1a469bff846f088f0b5bc8feb
Vadim Drobinin <vadim@drobinin.com>
2017-03-05 18:43:15 +0300

    Add custom cells for collection view and connect them with Main.storyboard

---
+++ CollectionViewChatDemo/RestaurantCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantCollectionViewCell.swift
+//  CollectionViewChatDemo
+//
+//  Created by Vadim Drobinin on 5/3/17.
+//  Copyright © 2017 Vadim Drobinin. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
